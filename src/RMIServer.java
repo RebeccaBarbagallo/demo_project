ba88a92f6a8ce6533910481ae8618d134722f37d
RebeccaBarbagallo <rebycart@hotmail.it>
2020-05-21 10:31:50 +0200

    add RMI commit

--- src/RMIServer.java
+++ src/RMIServer.java
@@ -1,7 +1,9 @@
 import java.net.MalformedURLException;
         import java.rmi.Naming;
         import java.rmi.RemoteException;
-        import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
         import java.util.ArrayList;
         import java.util.Date;
 
@@ -62,10 +64,14 @@ public class RMIServer extends UnicastRemoteObject implements RMIServices {
     public static void main(String args[]) {
         try {
             RMIServices services = new RMIServer();
-            Naming.rebind("listserver",services);
+            /*for local test
+            Naming.rebind("listserver",services);*/
+            System.setProperty("java.rmi.server.hostname","whitelodge.ns0.it");
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind("rmiservices", services);
+
+
         } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
